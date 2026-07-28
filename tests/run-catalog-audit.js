b6977e3389ed646521d7b3fa14c332ea043d54ff
victork4314-sys <victork4314@gmail.com>
2026-07-28 02:52:18 +0200

    Run catalog audit through production metadata corrections

---
+++ tests/run-catalog-audit.js
@@ -0,0 +1,6 @@
+"use strict";
+global.window = global;
+require("../smufl-catalog.js");
+require("../notation-catalog-core.js");
+require("../notation-catalog-metadata-corrections.js");
+require("./catalog-audit.test.js");
